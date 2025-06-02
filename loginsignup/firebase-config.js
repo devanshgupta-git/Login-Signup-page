89fef89399285b0519fd798c6dd1918431784756
devanshgupta-git <devansh.gupta1311@gmail.com>
2025-06-02 15:31:07 +0530

    Add files via upload

---
+++ loginsignup/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDIimPJSOXnnMvtfIKGMOL8JulDcke5Odw",
+  authDomain: "login-35d52.firebaseapp.com",
+  projectId: "login-35d52",
+  storageBucket: "login-35d52.firebasestorage.app",
+  messagingSenderId: "785094877492",
+  appId: "1:785094877492:web:50086c936163fd201ec561",
+  measurementId: "G-P1XKDYJ494"
+};
+
+//initializing firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
